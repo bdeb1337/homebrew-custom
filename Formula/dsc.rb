e2ca799920ac8346bdc5ac74f025272fe30d57ca
bdeb1337 <bdeb1337@jng.li>
2025-01-19 22:13:23 +0100

    moving away from the version and arch vars

--- Formula/dsc.rb
+++ Formula/dsc.rb
@@ -11,25 +11,21 @@ class Dsc < Formula
 
   on_macos do
     if Hardware::CPU.intel?
-      arch = "x86_64-apple-darwin"
+      url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.8/DSC-3.0.0-preview.8-x86_64-apple-darwin.tar.gz"
       sha256 "fc3aaa3f0a36f48d220726895bbf8a3375c43a926df3d3c57be873313846eeef"
-      url "https://github.com/PowerShell/DSC/releases/download/v#{version}/DSC-#{version}-#{arch}.tar.gz"
     elsif Hardware::CPU.arm?
-      arch = "aarch64-apple-darwin"
+      url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.8/DSC-3.0.0-preview.8-aarch64-apple-darwin.tar.gz"
       sha256 "02378b85bde208238acc55290ef5b1aa3b5ca884ad514d7a6bc01334c287274c"
-      url "https://github.com/PowerShell/DSC/releases/download/v#{version}/DSC-#{version}-#{arch}.tar.gz"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      arch = "x86_64-unknown-linux-gnu"
+      url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.8/DSC-3.0.0-preview.8-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "677264514d58b339f75795d2627f8c288642c096db8c6c544fd673d684044e15"
-      url "https://github.com/PowerShell/DSC/releases/download/v#{version}/DSC-#{version}-#{arch}.tar.gz"
     elsif Hardware::CPU.arm?
-      arch = "aarch64-unknown-linux-gnu"
+      url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.8/DSC-3.0.0-preview.8-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "668f15f564655145b3c9448c5c4c410521a777faa09117b36cccd94e00165f80"
-      url "https://github.com/PowerShell/DSC/releases/download/v#{version}/DSC-#{version}-#{arch}.tar.gz"
     end
   end
 
