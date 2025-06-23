f5cf6f56c9c4de8e6bd42bc14833704faf5e306e
bdeb1337 <bdeb1337@jng.li>
2025-06-23 13:36:58 +0200

    Bump dsc

--- Formula/dsc.rb
+++ Formula/dsc.rb
@@ -1,31 +1,31 @@
 class Dsc < Formula
   desc "Microsoft Desired State Configuration v3"
   homepage "https://github.com/PowerShell/DSC/releases"
-  version "3.0.0-preview.12"
+  version "3.1.0"
 
   livecheck do
     url :homepage
-    strategy :page_match
-    regex(%r{href=.*?/releases/tag/v?(\d+(?:\.\d+)+-preview\.\d+)}i)
+    strategy :github_latest
+    regex(%r{href=.*?/releases/tag/v?(\d+(?:\.\d+)+)}i)
   end
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.12/DSC-3.0.0-preview.12-x86_64-apple-darwin.tar.gz"
-      sha256 "82c1c2f8601837b6f7da552991fb41d500a2b7b163dafd3d7042fe20c433ece9"
+      url "https://github.com/PowerShell/DSC/releases/download/v3.1.0/DSC-3.1.0-x86_64-apple-darwin.tar.gz"
+      sha256 "1a1772fde7fee00b12c4879164cdc1df02001db0a8d6d01a1454e888e5bd3a89"
     elsif Hardware::CPU.arm?
-      url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.12/DSC-3.0.0-preview.12-aarch64-apple-darwin.tar.gz"
-      sha256 "ead49905c5b82bd2d859a8363238ce986f7ee0d5454e6013d3a1f50afde4c326"
+      url "https://github.com/PowerShell/DSC/releases/download/v3.1.0/DSC-3.1.0-aarch64-apple-darwin.tar.gz"
+      sha256 "a1c5bce4739af27e55811a5aafe75090cd1384b9e7b5054d63eaca37b6d3d691"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.12/DSC-3.0.0-preview.12-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "49a5cd2c5a6fb269f7480b91a2c3ecdbb9ea37a313a7e8adb1d0b2f93bc5fbd1"
+      url "https://github.com/PowerShell/DSC/releases/download/v3.1.0/DSC-3.1.0-x86_64-linux.tar.gz"
+      sha256 "7b2fc0d8e50e35dcd8cb45d63e7d461f4a3023becaf493b0d235cce6dd3004dd"
     elsif Hardware::CPU.arm?
-      url "https://github.com/PowerShell/DSC/releases/download/v3.0.0-preview.12/DSC-3.0.0-preview.12-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "f0a66c784530064af0c94e2de5f075fe79e318803909a4f36a57e1997f32dfe6"
+      url "https://github.com/PowerShell/DSC/releases/download/v3.1.0/DSC-3.1.0-aarch64-linux.tar.gz"
+      sha256 "cd00859739c5b5b9bd3625909014cb23ad13d7536f864c6f3fd9cf6022c6d533"
     end
   end
 
